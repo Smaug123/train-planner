5a78762c75f90f124a955d0a99c86f182a9db1ff
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2026-04-06 10:40:24 +0100

    Bump flake (#30)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Train journey planner";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
     rust-overlay = {
